b60107f57a5714605ca434760955fca0a13a1b0d
kcgreenn <kcgreenn@gmail.com>
2020-12-09 12:54:24 -0500

    Add main scene view and controller

---
+++ src/schedulingapp/SchedulingApp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedulingapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kcgre
+ */
+public class SchedulingApp extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/SchedulingApp.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Appointment Scheduling Application");
+        
+        stage.show();
+    }
+    
+}
